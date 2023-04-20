eb0f7464b1a72669967468a3ce114080bfc51b41
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-20 19:32:01 +0200

    a

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
 contract Raffle {
