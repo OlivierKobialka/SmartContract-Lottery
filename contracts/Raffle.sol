77796e4f37f3c972287121ddc3ac5ac92576eeca
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-20 19:36:04 +0200

    auto

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -5,7 +5,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     //  State variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -13,7 +13,10 @@ contract Raffle {
     // Events
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -31,7 +34,10 @@ contract Raffle {
         // request random number from Chainlink VRF | 2 transaction process
     }
 
-    function fulfillRandomWords() internal override {}
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
