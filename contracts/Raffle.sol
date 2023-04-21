194de26c5e372e2ddbde25dd6a59e47f1558d613
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-21 09:52:17 +0200

    fix

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,6 +9,7 @@ error Raffle__NotEnoughETHEntered();
 contract Raffle is VRFConsumerBaseV2 {
     //  State variables
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
     address payable[] private s_players;
     VRFCoordintaorV2Interface private immutable i_vrfCoordinator;
     bytes32 private immutable i_gasLane
@@ -19,6 +20,7 @@ contract Raffle is VRFConsumerBaseV2 {
 
     // Events
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId); 
 
     constructor(
         address vrfCoordinatorV2,
@@ -26,12 +28,14 @@ contract Raffle is VRFConsumerBaseV2 {
         bytes32 gasLane,
         uint64 subscriptionId,
         uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_vrfCoordinator = VRFCoordintaorV2Interface(vrfCoordinator);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        i_interval = interval;
     }
 
     function enterRaffle() public payable {
@@ -46,13 +50,14 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function requestRandomWinner() external {
         // request random number from Chainlink VRF | 2 transaction process
-        i_vrfCoordinator.requestRandomWords(
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
                 i_gasLane, // max price for payment for gwei
                 i_subscriptionId, // subscription id
                 REQUEST_CONFIRMATIONS, // minimum number of confirmations on chainlink node
                 i_callbackGasLimit, // gas limit for callback
                 NUM_WORDS // number of random words to return
-            )
+            );
+            emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(
