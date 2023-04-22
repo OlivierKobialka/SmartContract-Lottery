ff7eb3a49e864710bce8086c130e774d82be13f9
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-22 20:56:16 +0200

    a

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,10 +1,11 @@
 const { network, companionNetworks } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat.config");
 
 module.exports = async function ({ getNamedAccoutns, deployments }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    
+    if (developmentChains.includes(network.name)) { }
 
     const raffle = await deploy("Raffle", {
         from: deployer,
