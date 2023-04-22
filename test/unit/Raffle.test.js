0c490937900f6e8f8ed71301a17154036d44a0b9
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-22 21:28:14 +0200

    new consts

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,16 +1,27 @@
 const { getNamedAccounts, deployments } = require("hardhat");
-const { developmentChains } = require("../../helper-hardhat.config");
+const { developmentChains, networkConfig } = require("../../helper-hardhat.config");
 const { ethers } = require("hardhat");
+const { assert } = require("chai");
 
 !developmentChains.includes(network.name)
     ? descirbe.skip
     : describe("Raffle", async function () {
         let raffle, vrfCoordinatorV2Mock;
+        const chainId = network.config.chainId;
 
         beforeEach(async function () {
             const { deployer } = await getNamedAccounts();
             await deployments.fixture(["all"]);
             raffle = await ethers.getContract("Raffle", deployer)
             raffle = await ethers.getContract("vrfCoordinatorV2Mock", deployer)
+        })
+
+        descirbe("Constructor", async function () {
+            it('initializes the Raffle correctly', async function () {
+                const raffleState = await raffle.getRaffleState();
+                const interval = await raffle.getInterval();
+                assert.equal(reffleState.toString(), "0")
+                assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+            })
         })
     })
