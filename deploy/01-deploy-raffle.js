6fb9998f842557646509f92882419a2a8551ec46
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-22 20:57:13 +0200

    mock address

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -4,12 +4,16 @@ const { developmentChains } = require("../helper-hardhat.config");
 module.exports = async function ({ getNamedAccoutns, deployments }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
+    let vrfCoordinatorV2Address = 
 
-    if (developmentChains.includes(network.name)) { }
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("vrfCoordinatorV2Mock");
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    }
 
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfiramtions || 1
     })
