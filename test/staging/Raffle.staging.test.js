10313d7c44244ce434c88ee8b0920d1536dc9109
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-24 21:36:57 +0200

    new

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -14,6 +14,29 @@ developmentChains.includes(network.name)
         })
 
         describe("fullfillRandomWords", function () {
-            isCallTrace("works with live ChaainLink Keepers and chainlink VRF, we get a roandom winner")
+            isCallTrace("works with live ChaainLink Keepers and chainlink VRF, we get a roandom winner", async function () {
+                // enter the raffle
+                const startingTimeStamp = await raffle.getLatestTimeStamp()
+                const accounts = await ethers.getSigner()
+
+                // await raffle.enterRaffle({ value: raffleEntranceFee })
+                await new Promise(async (resolve, reject) => {
+                    raffle.once("WinnerPicked", async () => {
+                        console.log('Winner Picked event fired!');
+                        try {
+                            const recentWinner = await raffle.getRecentWinner()
+                            const raffleState = await raffle.getRaffleState()
+                            const einnerBalance = await raffle.getRaffleBalance()
+                        } catch (error) {
+                            console.log(error);
+                            reject(error)
+                        }
+                    })
+
+                    await raffle.enterRaffle({ value: raffleEntranceFee })
+
+
+                })
+            })
         })
     })
