888ec8a2c15661fb61b418c4961dce73498999f6
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-24 21:26:37 +0200

    clean

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -2,17 +2,14 @@ const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
-describe("Raffle Unit Tests", function () {
-    let raffle, raffleContract, vrfCoordinatorV2Mock, raffleEntranceFee, interval, player
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", function () {
+        let raffle, raffleContract, vrfCoordinatorV2Mock, raffleEntranceFee, interval, player
 
-    beforeEach(async () => {
-        accounts = await ethers.getSigners()
-        player = accounts[1]
-        await deployments.fixture(["mocks", "raffle"])
-        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-        raffleContract = await ethers.getContract("Raffle")
-        raffle = raffleContract.connect(player)
-        raffleEntranceFee = await raffle.getEntranceFee()
-        interval = await raffle.getInterval()
+        beforeEach(async () => {
+            accounts = await ethers.getSigners()
+            raffle = raffleContract.connect(player)
+            raffleEntranceFee = await raffle.getEntranceFee()
+        })
     })
-})
