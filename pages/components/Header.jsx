ab8fbbe3b8165ca3dc0791a635f309764e7ab4f5
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-27 20:49:39 +0200

    update useeffect

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -5,7 +5,13 @@ export default function Header() {
 	const { enableWeb3, account, isWeb3Enabled } = useMoralis();
 
 	useEffect(() => {
-		console.log(isWeb3Enabled);
+		if (isWeb3Enabled) return;
+		if (typeof window !== "undefined") {
+			if (window.localStorage.getItem("connected")) {
+				enableWeb3();
+			}
+		}
+		enableWeb3();
 	}, [isWeb3Enabled]);
 
 	return (
@@ -19,6 +25,9 @@ export default function Header() {
 					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
 					onClick={async () => {
 						await enableWeb3();
+						if (typeof window !== "undefined") {
+							window.localStorage.setItem("connected", "injected");
+						}
 					}}>
 					Connect Wallet
 				</button>
