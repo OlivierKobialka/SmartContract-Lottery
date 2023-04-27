110c0ee0903101c13d2c63aa503f265014f77898
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-27 21:40:04 +0200

    starter

--- pages/components/LotteryEntrance.jsx
+++ pages/components/LotteryEntrance.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useWeb3Contract } from "react-moralis";
 
-const LotteryEntrance = () => {
-  return (
-    <div>LotteryEntrance</div>
-  )
+export default function LotteryEntrance() {
+
+    // const {runContractFunction: enterRaffle} = useWeb3Contract({
+    //     abi:
+    //     contractAddress:
+    //     functionName:
+    //     params: {}
+    //     msgValue:
+    // })
+
+	return <div>LotteryEntrance</div>;
 }
-
-export default LotteryEntrance
