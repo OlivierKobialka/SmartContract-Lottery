f3e6e76489cce39e200a7570c4255ffb49fbb9c1
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-27 20:34:18 +0200

    set up eff

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,7 +1,28 @@
 import { useMoralis } from "react-moralis";
+import { useEffect } from "react";
 
 export default function Header() {
-	const { enableWeb3 } = useMoralis();
+	const { enableWeb3, account, isWeb3Enabled } = useMoralis();
 
-	return <div>Header</div>;
+	useEffect(() => {
+		console.log(isWeb3Enabled);
+	}, [isWeb3Enabled]);
+
+	return (
+		<div>
+			{account ? (
+				<div className='bg-blue-500 hover:bg-blue-700 w-44 text-center text-white font-bold py-2 px-4 rounded'>
+					Connected to {account.slice(0, 6)}...{account.slice(-4)}
+				</div>
+			) : (
+				<button
+					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+					onClick={async () => {
+						await enableWeb3();
+					}}>
+					Connect Wallet
+				</button>
+			)}
+		</div>
+	);
 }
