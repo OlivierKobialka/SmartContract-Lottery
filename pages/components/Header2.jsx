2812617e7fe5517e84ccca53f97c479f4c318a19
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-29 11:24:35 +0200

    fix in styling

--- pages/components/Header2.jsx
+++ pages/components/Header2.jsx
@@ -2,7 +2,7 @@ import { ConnectButton } from "web3uikit";
 
 export default function Header2() {
 	return (
-		<div className='flex w-full items-center justify-around'>
+		<div className='flex w-full items-center justify-around border-b-gray-200 border'>
 			<h1 className='text-4xl font-bold text-center my-10'>
 				Decentralized Lottery
 			</h1>
--- pages/components/LotteryEntrance.jsx
+++ pages/components/LotteryEntrance.jsx
@@ -1,14 +1,16 @@
 import { useWeb3Contract } from "react-moralis";
+import { abi, contractAddress } from "../../constants";
+import { useMoralis } from "react-moralis";
 
 export default function LotteryEntrance() {
 
-    // const {runContractFunction: enterRaffle} = useWeb3Contract({
-    //     abi:
-    //     contractAddress:
-    //     functionName:
-    //     params: {}
-    //     msgValue:
-    // })
+	// const {runContractFunction: enterRaffle} = useWeb3Contract({
+	//     abi: abi,
+	//     contractAddress: contractAddress,
+	//     functionName: "enterRaffle",
+	//     params: {},
+	//     msgValue:,
+	// })
 
 	return <div>LotteryEntrance</div>;
 }
